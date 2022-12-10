d640bdf3bfc65943b406cbfb498a0e39c311217f
MRPrio <maksudurprio.mp@gmail.com>
2022-12-10 17:32:37 +0600

    components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
+import Portfolio from './components/Portfolio/Portfolio';
+import Services from './components/Services/Services';
 
 function App() {
   return (
-    <div>
-      <h1>app</h1>
-    </div>
+    <>
+    <Header></Header>
+    <Nav></Nav>
+    <About></About>
+    <Services></Services>
+    <Portfolio></Portfolio>
+    <Contact></Contact>
+    <Footer></Footer>
+    </>
   );
 }
 
---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return (
+        <div>
+            <h3>This is Portfolio</h3>
+        </div>
+    );
+};
+
+export default Portfolio;
