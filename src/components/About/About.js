15e943bc6304528b34c6b015dbce402faf41029c
MRPrio <maksudurprio.mp@gmail.com>
2022-12-11 18:55:10 +0600

    2 animation added on two section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,27 +1,49 @@
-import React from 'react';
-import ME from '../../assets/me.png'
-import './About.css'
+import React, { useEffect } from "react";
+import ME from "../../assets/me.png";
+import "./About.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 const About = () => {
-    return (
-        <section id='about'>
-            <h5>Get to know</h5>
-            <h2>About Me</h2>
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  return (
+    <section
+      data-aos="fade-up-right"
+      data-aos-offset="200"
+      data-aos-delay="50"
+      data-aos-duration="1000"
+      data-aos-easing="ease-in-out"
+      data-aos-mirror="true"
+      data-aos-once="false"
+      data-aos-anchor-placement="top-center"
+      id="about"
+    >
+      <h5>Get to know</h5>
+      <h2>About Me</h2>
 
-            <div className="container about-container">
-                <div className="about-me">
-                    <div className="about-me-image">
-                        <img src={ME} alt="" />
-                    </div>
-                </div>
-                <div className="about-content">
-                    <div className='about-content-btn'>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam sit deleniti hic praesentium doloribus soluta optio culpa error, quos nulla suscipit placeat facere tempora eligendi. Aperiam velit eaque dolorum dolor?</p>
-                       <a href="#contact" className='btn about-btn'>Let's Talk</a>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+      <div className="container about-container">
+        <div className="about-me">
+          <div className="about-me-image">
+            <img src={ME} alt="" />
+          </div>
+        </div>
+        <div className="about-content">
+          <div className="about-content-btn">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam sit
+              deleniti hic praesentium doloribus soluta optio culpa error, quos
+              nulla suscipit placeat facere tempora eligendi. Aperiam velit
+              eaque dolorum dolor?
+            </p>
+            <a href="#contact" className="btn about-btn">
+              Let's Talk
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,45 @@
-import React from 'react';
-import CTA from './CTA';
-import ME from '../../assets/me.png'
-import HeaderSocials from './HeaderSocials';
-import './Header.css'
-import DevTypeWriter from './DevTypeWriter/DevTypeWriter';
+import React, { useEffect } from "react";
+import CTA from "./CTA";
+import ME from "../../assets/me.png";
+import HeaderSocials from "./HeaderSocials";
+import "./Header.css";
+import DevTypeWriter from "./DevTypeWriter/DevTypeWriter";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Header = () => {
-    return (
-        <header>
-            <div className="container header-container">
-                <h5>Hello I'm</h5>
-                <h1>Maksudur Rahman</h1>
-                <DevTypeWriter></DevTypeWriter>
-                {/* <h5 className="text-light">MERN Stack Developer</h5> */}
-                <HeaderSocials></HeaderSocials>
-                <CTA></CTA>
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  return (
+    <header
+      data-aos="fade-down-right"
+      data-aos-offset="200"
+      data-aos-delay="50"
+      data-aos-duration="1000"
+      data-aos-easing="ease-in-out"
+      data-aos-mirror="true"
+      data-aos-once="false"
+      data-aos-anchor-placement="top-center"
+    >
+      <div className="container header-container">
+        <h5>Hello I'm</h5>
+        <h1>Maksudur Rahman</h1>
+        <DevTypeWriter></DevTypeWriter>
+        {/* <h5 className="text-light">MERN Stack Developer</h5> */}
+        <HeaderSocials></HeaderSocials>
+        <CTA></CTA>
 
-                <div className="me">
-                    <img src={ME} alt="img" />
-                </div>
+        <div className="me">
+          <img src={ME} alt="img" />
+        </div>
 
-                <a href="#contact" className='scroll_down'>Scroll Down</a>
-            </div>
-        </header>
-    );
+        <a href="#contact" className="scroll_down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
