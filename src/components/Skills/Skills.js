640bfda6e65507136799f33750342fd1648fee37
MRPrio <maksudurprio.mp@gmail.com>
2022-12-11 19:16:33 +0600

    animation added for 3 section

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,30 +1,44 @@
-import React from 'react';
-import BackSkillbar from './FrontSkillbar/BackSkillbar';
-import FrontSkillbar from './FrontSkillbar/FrontSkillbar';
-import './Skills.css'
+import React, { useEffect } from "react";
+import BackSkillbar from "./FrontSkillbar/BackSkillbar";
+import FrontSkillbar from "./FrontSkillbar/FrontSkillbar";
+import "./Skills.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Skills = () => {
-    return (
-        <section id='skills'>
-            <h5>What Skills I Have</h5>
-            <h2>My Skills</h2>
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  return (
+    <section
+      data-aos-offset="200"
+      data-aos-delay="50"
+      data-aos-duration="1000"
+      data-aos-easing="ease-in-out"
+      data-aos-mirror="true"
+      data-aos-once="false"
+      data-aos-anchor-placement="top-center"
+      id="skills"
+    >
+      <h5>What Skills I Have</h5>
+      <h2>My Skills</h2>
 
-            <div className='container skills-container'>
-                <div className="skill-front-end">
-                    <h3 className='skill-side-name'>FrontEnd Development</h3>
-                    <div>
-                        <FrontSkillbar></FrontSkillbar>
-                    </div>
-                </div>
-                <div className="skill-front-end">
-                    <h3 className='skill-side-name'>BackEnd Development</h3>
-                    <div>
-                        <BackSkillbar></BackSkillbar>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+      <div className="container skills-container">
+        <div  data-aos="slide-right" className="skill-front-end">
+          <h3 className="skill-side-name">FrontEnd Development</h3>
+          <div>
+            <FrontSkillbar></FrontSkillbar>
+          </div>
+        </div>
+        <div data-aos="slide-down" className="skill-front-end">
+          <h3 className="skill-side-name">BackEnd Development</h3>
+          <div>
+            <BackSkillbar></BackSkillbar>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Skills;
