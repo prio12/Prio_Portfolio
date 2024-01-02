852b19c7a66542905f4882b5e25cc95c0362d346
MRPrio <maksudurprio.mp@gmail.com>
2024-01-02 23:14:29 +0600

    added firebase to deploy

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4RAMo8dTWWeswpVgpKtf7jhIOaYZPX5U",
+  authDomain: "portfolio-1df1e.firebaseapp.com",
+  projectId: "portfolio-1df1e",
+  storageBucket: "portfolio-1df1e.appspot.com",
+  messagingSenderId: "576140392791",
+  appId: "1:576140392791:web:57d9e0ab9333be60aea612"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
